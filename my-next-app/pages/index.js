afa4665e18f5ba210f236087a90f24cb6bdbfefd
ARAVIND K <163990307+Aravindrio2@users.noreply.github.com>
2025-12-09 15:23:20 +0530

    Add files via upload

---
+++ my-next-app/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px", fontSize: "24px" }}>
+      Hi, this is Aravind. Welcome to my world.
+    </div>
+  )
+}
